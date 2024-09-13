29e304deaf7e867e093ea78f0ef3a2f068d9537f
Peter Wirth <57559346+emax-77@users.noreply.github.com>
2024-09-13 11:23:33 +0200

    add a profile model’s fields to the user page in the admin on admin.py in auth app

--- The_Canteen/authentication/admin.py
+++ The_Canteen/authentication/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
 
-# Register your models here.
+from authentication.models import Member
+
+
+# Define an inline admin descriptor for Member model
+# which acts a bit like a singleton
+class MemberInline(admin.StackedInline):
+    model = Member
+    can_delete = False
+    verbose_name_plural = "members"
+
+
+# Define a new User admin
+class UserAdmin(BaseUserAdmin):
+    inlines = [MemberInline]
+
+
+# Re-register UserAdmin
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
