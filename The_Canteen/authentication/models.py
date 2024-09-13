b2d9cbb7ecbedf5cc637d9b65b672a3833172888
Peter Wirth <57559346+emax-77@users.noreply.github.com>
2024-09-13 12:53:46 +0200

    models.py in auth app deleted

--- The_Canteen/authentication/models.py
+++ The_Canteen/authentication/models.py
@@ -1,11 +1,2 @@
 from django.db import models
-from django.contrib.auth.models import User
-
-class Member(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    phone = models.CharField(max_length=255, null=True)
-    address = models.CharField(max_length=255,null=True)
-    zipcode = models.CharField(max_length=255,null=True)
-    city = models.CharField(max_length=255,null=True)
-    country = models.CharField(max_length=255,null=True)
-    
+# Create your models here.
