9058b81177f074a3d7373807bcefc2cfaf13ff8d
Peter Wirth <57559346+emax-77@users.noreply.github.com>
2024-09-13 13:06:24 +0200

    Extend the default User with Customer model in canteen app

--- The_Canteen/canteen/admin.py
+++ The_Canteen/canteen/admin.py
@@ -7,3 +7,24 @@ admin.site.register(Order)
 admin.site.register(OrderItem)
 admin.site.register(Payment)
 admin.site.register(Delivery)
+
+
+# Extend the default User with Customer model
+
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
+
+# Define an inline admin descriptor for Customer model
+# which acts a bit like a singleton
+class CustomerInline(admin.StackedInline):
+    model = Customer
+    can_delete = False
+    verbose_name_plural = "customers"
+
+# Define a new User admin
+class UserAdmin(BaseUserAdmin):
+    inlines = [CustomerInline]
+
+# Re-register UserAdmin
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
