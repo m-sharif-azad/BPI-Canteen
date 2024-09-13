33aa2fa569784bb797e162e462f4da2c916fd7c0
Peter Wirth <57559346+emax-77@users.noreply.github.com>
2024-09-13 11:19:43 +0200

    extend the default User model on models.py in auth app

--- The_Canteen/authentication/models.py
+++ The_Canteen/authentication/models.py
@@ -1,2 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import User
+
+class Member(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    phone = models.CharField(max_length=255, null=True)
+    address = models.CharField(max_length=255,null=True)
+    zipcode = models.CharField(max_length=255,null=True)
+    city = models.CharField(max_length=255,null=True)
+    country = models.CharField(max_length=255,null=True)
+    
