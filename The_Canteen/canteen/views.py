8ad7577c0bce4f1472fb544272d605897922239d
Peter Wirth <57559346+emax-77@users.noreply.github.com>
2024-09-14 16:54:05 +0200

    new pages deffined on views.py in canteen app

--- The_Canteen/canteen/views.py
+++ The_Canteen/canteen/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+
 
 # Create your views here.
+
+# test page
+def testing(request):
+    template = loader.get_template('template.html')
+    return HttpResponse(template.render(request=request))
+
+def order(request):
+    template = loader.get_template('order.html')
+    return HttpResponse(template.render(request=request))
