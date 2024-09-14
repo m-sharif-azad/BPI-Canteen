6221423d940e575e7ae003aa3a6ee93770f7e462
Peter Wirth <57559346+emax-77@users.noreply.github.com>
2024-09-14 17:12:24 +0200

    contact page defined on canteen/views.py

--- The_Canteen/canteen/views.py
+++ The_Canteen/canteen/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from django.core.mail import send_mail
+from django.conf import settings
 
 
 # Create your views here.
@@ -10,6 +12,30 @@ def testing(request):
     template = loader.get_template('testing.html')
     return HttpResponse(template.render(request=request))
 
+# order page
 def order(request):
     template = loader.get_template('order.html')
     return HttpResponse(template.render(request=request))
+
+# contact page
+def contact(request):
+    template = loader.get_template('contact.html')
+    context = {}
+
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        message_content = request.POST.get('message')
+
+        subject = f'Message from Cycling Club, user: {name}'
+        message = f'{name} ({email}) wrote:\n\n{message_content}'
+        email_from = settings.EMAIL_HOST_USER
+        recipient_list = ['peter.wirth@gmail.com']
+
+        # Send the email
+        send_mail(subject, message, email_from, recipient_list)
+
+        # Success message
+        context['success_message'] = "Thank you! Your message has been sent successfully."
+
+    return HttpResponse(template.render(context, request))
