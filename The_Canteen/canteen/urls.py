63c46cd66ddb49048b9d9ec54372e4af82fb0eda
Peter Wirth <57559346+emax-77@users.noreply.github.com>
2024-09-14 17:00:55 +0200

    top_nav deleted from urlpatterns on canteen/urls.py

--- The_Canteen/canteen/urls.py
+++ The_Canteen/canteen/urls.py
@@ -3,7 +3,6 @@ from . import views
 
 
 urlpatterns = [
-	path('top_nav/', views.top_nav, name='top_nav'), # Top Navigation bar
     path('order/', views.order, name="order"),	 # Home page
     path('testing/', views.testing, name="testing"), # Test page
 ]
