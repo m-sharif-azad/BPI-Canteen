c06d86bb5c89eb6ef04dea4821803b122f87bbf9
Peter Wirth <57559346+emax-77@users.noreply.github.com>
2024-09-14 17:07:43 +0200

    wrong html name fix on canteen/views.py

--- The_Canteen/canteen/views.py
+++ The_Canteen/canteen/views.py
@@ -7,7 +7,7 @@ from django.template import loader
 
 # test page
 def testing(request):
-    template = loader.get_template('template.html')
+    template = loader.get_template('testing.html')
     return HttpResponse(template.render(request=request))
 
 def order(request):
