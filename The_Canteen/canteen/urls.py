fb13f2341d0caf3d5ea4514bcb8153da70c4960a
Peter Wirth <57559346+emax-77@users.noreply.github.com>
2024-09-16 09:36:38 +0200

    basket added to urlpatterns on urls.py in canteen app

--- The_Canteen/canteen/urls.py
+++ The_Canteen/canteen/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('order/', views.order, name="order"),	 # Home page
     path('testing/', views.testing, name="testing"), # Test page
     path('contact/', views.contact, name="contact"), # Contact page
+    path('basket/', views.basket, name="basket"), # Shopping Basket page
 ]
 
