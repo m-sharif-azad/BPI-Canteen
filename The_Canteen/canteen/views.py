46a71d0457e9cffd8a6aaf6489db9bce385ee9eb
Peter Wirth <57559346+emax-77@users.noreply.github.com>
2024-09-18 14:20:02 +0200

    total_foof_price value created in def order on views.py

--- The_Canteen/canteen/views.py
+++ The_Canteen/canteen/views.py
@@ -11,18 +11,20 @@ from django.contrib.auth.decorators import login_required
 # order page
 def order(request):
     if request.method == "POST":
-        delivery_fee = 3
+        delivery_fee = 2
+        total_price  = 0
         selected_items_ids = request.POST.getlist('selected_items')
         selected_items = MenuItem.objects.filter(id__in=selected_items_ids)
-        total_price = sum(item.price for item in selected_items)
+        total_food_price = sum(item.price for item in selected_items)
         customer = Customer.objects.get(user=request.user)
 
         # Redirect to basket.html page and pass selected items
         return render(request, 'basket.html', {
             'selected_items': selected_items,
-            'total_price': total_price,
+            'total_price': total_food_price,
             'customer': customer,
-            'delivery_fee': delivery_fee
+            'delivery_fee': delivery_fee,
+            'total_price':total_price
             })
     
     menuitems = MenuItem.objects.all()
