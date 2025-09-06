7507ab514420e144856ec941fadf3357e6bc5bad
Md Sharif Uddin Azad <69681947+m-sharif-azad@users.noreply.github.com>
2025-09-07 01:31:38 +0600

    Update asgi.py

--- The_Canteen/The_Canteen/asgi.py
+++ The_Canteen/The_Canteen/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'The_Canteen.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'BPI_Canteen.settings')
 
 application = get_asgi_application()
